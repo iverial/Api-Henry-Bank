ae205b7d4a519a27aeabed0e81ce70385d9262fd
briangvazq <briangvazq@gmail.com>
2022-07-06 16:43:36 -0300

    fix

--- src/models/Crypto.js
+++ src/models/Crypto.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('nombreTabla', {
+  sequelize.define('crypto', {
     hash: {
       type: DataTypes.STRING,
       primaryKey: true,
