a61d9463932acce0bcfb770008586bc8a891680d
briangvazq <briangvazq@gmail.com>
2022-07-06 16:45:52 -0300

    Fixed Caps

--- src/models/Crypto.js
+++ src/models/Crypto.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('crypto', {
+  sequelize.define('Crypto', {
     hash: {
       type: DataTypes.STRING,
       primaryKey: true,
--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,7 +1,7 @@
 const DataTypes = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('transaction', {
+  sequelize.define('Transaction', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
