f1c4f6ab6e8679609b8046ce88772ac7a705bf3c
briangvazq <briangvazq@gmail.com>
2022-07-08 18:10:47 -0300

    Added Account

--- src/db.js
+++ src/db.js
@@ -19,18 +19,18 @@ const modelDefiners = [];
 
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter(
-    file =>
+    (file) =>
       file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
   )
-  .forEach(file => {
+  .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map(entry => [
+let capsEntries = entries.map((entry) => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
   entry[1],
 ]);
@@ -39,38 +39,45 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 
-const { Account, Crypto, InvestTransaction, LockedStake, Nationality, User, Transaction, Stock } = sequelize.models;
+const {
+  Account,
+  Crypto,
+  InvestTransaction,
+  LockedStake,
+  Nationality,
+  User,
+  Transaction,
+  Stock,
+} = sequelize.models;
 
-//User -- Nationality
-User.hasMany(Nationality)
-Nationality.belongsTo(User)
+// An User can have only one Nationality, but Many users can have the same Nationality
+Nationality.belongsToMany(User);
+User.belongsToMany(Nationality);
+
 // User -- Account
-User.hasOne(Account)
-Account.belongsTo(User)
+User.hasOne(Account);
+Account.belongsTo(User);
 
 // Account.belongsToMany(Account ,{as: 'CBU_REMITENTE', foreignKey: 'id',  through: 'Transaction'})
 // Account.belongsToMany(Account ,{as: 'CBU_RECEPTOR', foreignKey: 'id',  through: 'Transaction'})
 
 // LockedStake --- Account
-LockedStake.hasMany(Account)
-Account.belongsTo(LockedStake)
+LockedStake.hasMany(Account);
+Account.belongsTo(LockedStake);
 
 // Account --- LockedStake
-Account.hasOne(LockedStake)
-LockedStake.belongsTo(Account)
+Account.hasOne(LockedStake);
+LockedStake.belongsTo(Account);
 
 // Account --- Crypto
-Account.belongsToMany(Crypto, {through: 'Account_Crypto'})
-Crypto.belongsToMany(Account, {through: 'Account_Crypto'})
+Account.belongsToMany(Crypto, { through: 'Account_Crypto' });
+Crypto.belongsToMany(Account, { through: 'Account_Crypto' });
 
 // Account --- Stock
-Account.belongsToMany(Stock, {through: 'Account_Stock'})
-Stock.belongsToMany(Account, {through: 'Account_Stock'})
+Account.belongsToMany(Stock, { through: 'Account_Stock' });
+Stock.belongsToMany(Account, { through: 'Account_Stock' });
 
-//relaciones 
-
-
-
+//relaciones
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
