0158dd9fda4fd0a5113cc6781d8972414a4d14e0
Maximiliano Rocha <maxi.jrocha97@gmail.com>
2022-07-12 18:34:14 -0300

    Add relation crypto with savingAccount

--- src/models/Crypto.js
+++ src/models/Crypto.js
@@ -1,33 +1,34 @@
 const { DataTypes } = require('sequelize');
 
-module.exports = (sequelize) => {
-  sequelize.define('Crypto', {
-    hash: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false,
-      unique: true,
+module.exports = sequelize => {
+  sequelize.define(
+    'Crypto',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      balance: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      simbol: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
     },
-    balance: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      defaultValue: 0,
-    },
-    buyPrice: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      defaultValue: 0,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-
-    simbol: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-  });
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
 };
