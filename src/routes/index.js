6ee6b5fa63c5de599fbdbafaa68b4f49b81e8cd1
Ivan <100495676+iverial@users.noreply.github.com>
2022-07-12 16:21:59 -0300

    ruta email

--- src/routes/index.js
+++ src/routes/index.js
@@ -31,6 +31,7 @@ const registerRouter = require('./register.router.js');
 const userRouter = require('./user.router.js');
 const lockedStake = require('./lockedStake.router.js');
 const cryptoRouter = require('./crypto.router.js');
+const userEmail = require('./userEmail.router')
 
 // const temperamentsRouter = require('./temperamentsRouter/router.js');
 
@@ -54,5 +55,6 @@ router.use(
   passport.authenticate('jwt', { session: false }),
   lockedStake
 );
+router.use('/userEmail', userEmail)
 
 module.exports = router;
