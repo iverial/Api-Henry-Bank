51e94de111f96e4dd0c2c0249824b7b86c130a3b
Maximiliano Rocha <maxi.jrocha97@gmail.com>
2022-07-20 10:45:04 -0300

    add admin/cryptos

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -4,10 +4,13 @@ const router = Router();
 const {
   registerLockedStake,
   registerTransaction,
+  registerCryptos,
 } = require('../controllers/admin.controller.js');
 
 router.get('/lockedStake', registerLockedStake);
 
 router.get('/transactions', registerTransaction);
+
+router.get('/cryptos', registerCryptos);
 
 module.exports = router;
